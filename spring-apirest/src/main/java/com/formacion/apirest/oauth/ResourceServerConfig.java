fa86370aac66f2893ce17cea62b9cd8c601dd4e1
Isabelino Rolando Lopez Roman <isabelino.rolando@gmail.com>
2022-11-17 11:33:57 +0100

    ultimos cambios

--- spring-apirest/src/main/java/com/formacion/apirest/oauth/ResourceServerConfig.java
+++ spring-apirest/src/main/java/com/formacion/apirest/oauth/ResourceServerConfig.java
@@ -1,5 +1,22 @@
 package com.formacion.apirest.oauth;
 
-public class ResourceServerConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/clientes").permitAll()
+		.anyRequest().authenticated();
+	}
+
+	
+	
 }
